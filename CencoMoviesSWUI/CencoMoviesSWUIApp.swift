28e62c969e9b49c5de45d3e87470fe5da1d18f31
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-01-15 20:39:45 -0300

    Initial Commit

---
+++ CencoMoviesSWUI/CencoMoviesSWUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CencoMoviesSWUIApp.swift
+//  CencoMoviesSWUI
+//
+//  Created by Jose Preatorian on 15-01-26.
+//
+
+import SwiftUI
+
+@main
+struct CencoMoviesSWUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
